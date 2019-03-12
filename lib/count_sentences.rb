67d8fa6eda815d25d216bea51af3e4b62ac72e45
Eyal Toledano <eyaltoledano@me.com>
2019-03-12 00:35:19 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,12 +16,12 @@ class String
 
   def count_sentences
     sentences = 0
-    binding.pry
     self.split.each do |item|
       if sentence? or question? or exclamation?
         sentences + 1
       end
     end
+    binding.pry
     sentences
   end
 end
